54bedb31d77952adff064eeffc6aef487ee4a1aa
Lalit Gour <lalitg@iitbhilai.ac.in>
2023-03-14 18:01:23 +0530

    popup.js: Store Password in cookies

--- popupScript.js
+++ popupScript.js
@@ -52,5 +52,28 @@ generatePasswordButton.addEventListener('click', () => {
   }
   
   passwordTextarea.value = password;
+  function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+  setCookie(0,password,5);
+  function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+  console.log(getCookie(0));
 });
   
